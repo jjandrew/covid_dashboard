1ed820ebc451c52b9241e6064a13b75b2781bd16
jjandrew <uwwnhayy>
2021-11-12 10:34:45 +0000

    Added Doc strings for covid_data_handler module as well as parse_csv_data function

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -1,6 +1,17 @@
+"""
+This Module processes the data received in the 'nation_2021-10-28.csv' file
+"""
+
+
 def parse_csv_data(csv_filename):
+    """
+    This function will read from the 'nation_2021-10-28' file and return a list of strings corresponding to each
+    line of the csv file.
+    :param csv_filename:
+    :return: List[str]
+    """
     data = []
-    with open (csv_filename, 'r') as f:
+    with open(csv_filename, 'r') as f:
         for row in f:
             data.append(row)
         return data
