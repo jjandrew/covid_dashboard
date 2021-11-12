03d5f0352f47bdf7bbf0d48eb11145c63161fd40
jjandrew <uwwnhayy>
2021-11-12 10:57:12 +0000

    Process_covid_csv_data created Weekly cases now calculates and returns correct value to pass test

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -13,5 +13,14 @@ def parse_csv_data(csv_filename):
     data = []
     with open(csv_filename, 'r') as f:
         for row in f:
-            data.append(row)
-        return data
+            data.append(row.rstrip())
+    return data
+
+
+def process_covid_csv_data(covid_csv_data):
+    week_cases = 0
+    for i in range(3, 10):
+        week_cases += int(covid_csv_data[i].split(',')[6])
+    hospital_cases = 0
+    total_deaths = 1
+    return week_cases, hospital_cases, total_deaths
--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 from covid_data_handler import parse_csv_data
+from covid_data_handler import process_covid_csv_data
 
 
-def test_parse_csv_data ():
+def test_parse_csv_data():
     data = parse_csv_data('nation_2021-10-28.csv')
     assert len(data) == 639
 
 
+def test_process_covid_csv_data():
+    last7days_cases, current_hospital_cases, total_deaths = process_covid_csv_data(parse_csv_data('nation_2021-10-28'
+                                                                                                  '.csv'))
+    assert last7days_cases == 240_299
+    assert current_hospital_cases == 0
+    assert total_deaths == 1
+
+
 test_parse_csv_data()
+test_process_covid_csv_data()
