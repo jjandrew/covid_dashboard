fc740e25f4a0d60b78859d35801f94bc7099e04b
jjandrew <uwwnhayy>
2021-11-12 11:12:24 +0000

    Process_covid_csv_data complete and tested Function now calculates hospital cases and total_deaths Doc string added to function

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -8,9 +8,10 @@ def parse_csv_data(csv_filename):
     This function will read from the 'nation_2021-10-28' file and return a list of strings corresponding to each
     line of the csv file.
     :param csv_filename:
-    :return: List[str]
+    :return: List[str] of each line of the csv file
     """
     data = []
+    # Iterates through the file and strips '\n' from each line before adding the line as a string to the data list
     with open(csv_filename, 'r') as f:
         for row in f:
             data.append(row.rstrip())
@@ -18,9 +19,23 @@ def parse_csv_data(csv_filename):
 
 
 def process_covid_csv_data(covid_csv_data):
+    """
+    Takes in csv data file as input and calculates:
+        Cases for last week excluding most recent value as is incomplete
+        Hospital cases
+        Cumulative Deaths as up to date as file allows
+    :param covid_csv_data:
+    :return: weekly cases, hospital cases, cumulative deaths as integers
+    """
+    # calculates weekly cases by summing last 7 entries
     week_cases = 0
     for i in range(3, 10):
         week_cases += int(covid_csv_data[i].split(',')[6])
-    hospital_cases = 0
-    total_deaths = 1
+    # calculates hospital cases by reading last entry to csv file
+    hospital_cases = int(covid_csv_data[1].split(',')[5])
+    # iterates through csv file until an entry for cumulative deaths is found
+    count = 1
+    while covid_csv_data[count].split(',')[4] == '':
+        count += 1
+    total_deaths = int(covid_csv_data[count].split(',')[4])
     return week_cases, hospital_cases, total_deaths
--- test.py
+++ test.py
@@ -11,8 +11,8 @@ def test_process_covid_csv_data():
     last7days_cases, current_hospital_cases, total_deaths = process_covid_csv_data(parse_csv_data('nation_2021-10-28'
                                                                                                   '.csv'))
     assert last7days_cases == 240_299
-    assert current_hospital_cases == 0
-    assert total_deaths == 1
+    assert current_hospital_cases == 7_019
+    assert total_deaths == 141_544
 
 
 test_parse_csv_data()
