e7d5a7103a006f41483202a16ad747cdebbc7599
jjandrew <uwwnhayy>
2021-11-15 17:10:11 +0000

    Update news function added

---
+++ covid_news_handling.py
@@ -0,0 +1,35 @@
+def news_API_request(covid_terms="Covid COVID-19 coronavirus"):
+    """
+    Takes in terms to be searched in the API as a string separated by spaces and returns an array of the responses
+    received from the various API calls
+    :param covid_terms: Takes in a string separated by spaces of terms to be searched for in the API
+    :return: Array of the responses received from the various API calls
+    """
+    import requests
+    from keys import get_newsapi_key
+
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = get_newsapi_key()
+    terms = covid_terms.split(" ")
+    responses = []
+    for term in terms:
+        complete_url = base_url + "q=" + term + "&apiKey=" + api_key
+        response = requests.get(complete_url)
+        responses.append(response.json())
+    return responses
+
+
+def update_news():
+    """
+    Retrieves api responses and loops through the articles in each of the responses appending each article to an array.
+    :return: articles returned from the various API calls
+    """
+    api_responses = news_API_request()
+    articles = []
+    for response in api_responses:
+        for article in response['articles']:
+            articles.append(article)
+    return articles
+
+
+update_news()
