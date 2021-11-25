4da38aea3a5efb0e6e5b45f1d35915a20e7c3ee0
jjandrew <uwwnhayy>
2021-11-25 15:07:08 +0000

    Changed index.html file and added methods to retrieve data from url

--- main.py
+++ main.py
@@ -11,13 +11,23 @@ s = sched.scheduler(time.time, time.sleep)
 articles = update_news()
 
 
-def scheduled_news_update():
+def news_update():
     global articles
     articles = update_news()
 
 
+def schedule_add_news():
+    event = s.enter(15, 1, news_update())
+
+
 @app.route('/index')
 def index():
+    s.run(blocking=False)
+    label_name = request.args.get('two')
+    if label_name:
+        # check scheduler_time exists
+        scheduler_time = request.arg.get('update')
+        schedule_add_news()
     return render_template('index.html', title='Daily Update', news_articles=articles)
 
 
